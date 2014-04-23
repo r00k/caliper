c5da82cbbfc9f7ba948890cabfb8fc1f8d4f0516
Ben Orenstein <ben.orenstein@gmail.com>
2014-04-22 23:18:41 -0500

    Remove roundtrip test

--- test/caliper/test/db.clj
+++ test/caliper/test/db.clj
@@ -12,12 +12,3 @@
             :date_of_accident #inst "2014-04-01T12:00:00-00:00"}
            (parse-dates {:date_of_birth "1983-04-16"
                          :date_of_accident "2014-04-01"})))))
-
-(deftest roundtrip
-  (testing "dates are maintained through a roundtrip"
-    ; TODO: why does this come out at 5am instead of noon?
-    (is (= #inst "1983-04-16T05:00:00"
-           (-> (create-client {:date_of_birth "1983-04-16"})
-               :id
-               find-client
-               :date_of_birth)))))
