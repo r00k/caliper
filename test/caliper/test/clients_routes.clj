ed48373146fd34dbd82a53927855962f0c282dbb
Ben Orenstein <ben.orenstein@gmail.com>
2014-05-04 15:12:10 -0400

    Create new test file

---
+++ test/caliper/test/clients_routes.clj
@@ -0,0 +1,20 @@
+(ns caliper.test.clients-routes
+  (:use clojure.test
+        ring.mock.request
+        caliper.handler)
+  (:require [caliper.models.db :as db]))
+
+(deftest test-client
+  (testing "GET /clients/new"
+    (let [response (app (request :get "/clients/new"))]
+      (is (= (:status response) 200))))
+  (testing "POST /clients"
+    (let [response (app (request :post "/clients" {:first_name "Ben"
+                                                   :last_name "Orenstein"
+                                                   :date_of_birth "1983-04-16"
+                                                   :date_of_accident "2014-04-01"}))]
+      (is (= (:status response) 200))
+      (is (re-find #"Ben" (:body response)))
+      (is (re-find #"Orenstein" (:body response)))
+      (is (re-find #"1983-04-16" (:body response)))
+      (is (re-find #"2014-04-01" (:body response))))))
