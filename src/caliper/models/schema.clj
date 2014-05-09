1f75d5468e418087389be4bc1f761a826b98f8fb
Ben Orenstein <ben.orenstein@gmail.com>
2014-05-09 12:38:12 -0400

    Extract fns

--- src/caliper/models/schema.clj
+++ src/caliper/models/schema.clj
@@ -1,16 +1,23 @@
 (ns caliper.models.schema)
 
-(def db-spec
-  (if (System/getenv "DATABASE_URL")
-    (let [db-uri (java.net.URI. (System/getenv "DATABASE_URL"))
-          user-and-password (clojure.string/split (.getUserInfo db-uri) #":")]
-      {:classname "org.postgresql.Driver"
-       :subprotocol "postgresql"
-       :user (get user-and-password 0)
-       :password (get user-and-password 1) ; may be nil
-       :subname (if (= -1 (.getPort db-uri))
-                  (format "//%s%s" (.getHost db-uri) (.getPath db-uri))
-                  (format "//%s:%s%s" (.getHost db-uri) (.getPort db-uri) (.getPath db-uri)))})
-    {:subprotocol "postgresql"
-     :subname "//localhost/caliper"}))
+(defn heroku-database-url->jdbc-connection-string
+  "Converts Heroku's DATABASE_URL to a JDBC connection string"
+  [heroku-database-url]
+  (let [db-uri (java.net.URI. heroku-database-url)
+        user-and-password (clojure.string/split (.getUserInfo db-uri) #":")]
+    {:classname "org.postgresql.Driver"
+     :subprotocol "postgresql"
+     :user (get user-and-password 0)
+     :password (get user-and-password 1) ; may be nil
+     :subname (if (= -1 (.getPort db-uri))
+                (format "//%s%s" (.getHost db-uri) (.getPath db-uri))
+                (format "//%s:%s%s" (.getHost db-uri) (.getPort db-uri) (.getPath db-uri)))}))
 
+(def local-database
+  {:subprotocol "postgresql"
+   :subname "//localhost/caliper"})
+
+(def db-spec
+  (if-let [database-url (System/getenv "DATABASE_URL")]
+    (heroku-database-url->jdbc-connection-string)
+    local-database))
