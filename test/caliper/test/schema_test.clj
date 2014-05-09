6ab3b89ca71ee637cbd4473e63b5fe8171d690b1
Ben Orenstein <ben.orenstein@gmail.com>
2014-05-09 13:20:31 -0400

    Simplify

--- test/caliper/test/schema_test.clj
+++ test/caliper/test/schema_test.clj
@@ -3,10 +3,9 @@
         caliper.models.schema))
 
 (deftest test-heroku-database-parsing
-  (testing "when all elements are present"
-    (is (= {:classname "org.postgresql.Driver"
-            :subprotocol "postgresql"
-            :user "user"
-            :password "pass"
-            :subname "//host:1234/path"}
-          (heroku-database-url->jdbc-connection-map "postgres://user:pass@host:1234/path")))))
+  (is (= {:classname "org.postgresql.Driver"
+          :subprotocol "postgresql"
+          :user "user"
+          :password "pass"
+          :subname "//host:1234/path"}
+         (heroku-database-url->jdbc-connection-map "postgres://user:pass@host:1234/path"))))
