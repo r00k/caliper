69747933d080042700c801ee46f4ea8faabdedbf
Ben Orenstein <ben.orenstein@gmail.com>
2014-05-09 12:01:00 -0400

    Extract more views

--- src/caliper/routes/clients.clj
+++ src/caliper/routes/clients.clj
@@ -9,31 +9,12 @@
 (defn new-client []
   (layout/render
     "app.html"
-    {:content (html (views/client-form))}))
+    {:content (views/client-form)}))
 
 (defn client-index []
   (layout/render
     "app.html"
-    {:content (let [clients (db/all-clients)]
-                (html
-                  [:h2 "Clients"]
-                  [:p
-                   [:a {:href "/clients/new" :id "new-client"} "Add new"]]
-                  [:br]
-                  [:table.table
-                   [:tr
-                    [:th "First name"]
-                    [:th "Last name"]
-                    [:th "DOB"]
-                    [:th "Date of accident"]
-                    [:th "Records depts"]]
-                   (for [{:keys [first_name last_name date_of_birth date_of_accident records_departments]} clients]
-                     [:tr
-                      [:td first_name]
-                      [:td last_name]
-                      [:td date_of_birth]
-                      [:td date_of_accident]
-                      [:td (map :department_title records_departments)]])]))}))
+    {:content (views/index)}))
 
 (defn create-client [params]
   (db/create-client params)
--- src/caliper/views/clients.clj
+++ src/caliper/views/clients.clj
@@ -12,25 +12,52 @@
       department_title]]))
 
 (defn client-form []
-  (form-to {:role "form"} [:post "/clients"]
-             [:legend "New client"]
-             [:div.form-group
-              [:label { :for "first_name" } "First name"]
-              (text-field {:class "form-control"} "first_name")]
+  (html
+    (form-to
+      {:role "form"} [:post "/clients"]
+      [:legend "New client"]
+      [:div.form-group
+       [:label { :for "first_name" } "First name"]
+       (text-field {:class "form-control"} "first_name")]
 
-             [:div.form-group
-              [:label { :for "last_name" } "Last name"]
-              (text-field {:class "form-control"} "last_name")]
+      [:div.form-group
+       [:label { :for "last_name" } "Last name"]
+       (text-field {:class "form-control"} "last_name")]
 
-             [:div.form-group
-              [:label { :for "date_of_birth" } "Date of birth"]
-              (text-field {:class "form-control" :placeholder "YYYY-MM-DD"} "date_of_birth")]
+      [:div.form-group
+       [:label { :for "date_of_birth" } "Date of birth"]
+       (text-field {:class "form-control" :placeholder "YYYY-MM-DD"} "date_of_birth")]
 
-             [:div.form-group
-              [:label { :for "date_of_accident" } "Date of accident"]
-              (text-field {:class "form-control" :placeholder "YYYY-MM-DD"} "date_of_accident")]
+      [:div.form-group
+       [:label { :for "date_of_accident" } "Date of accident"]
+       (text-field {:class "form-control" :placeholder "YYYY-MM-DD"} "date_of_accident")]
 
-             [:label {:for "treatments"} "Records Departments"]
-             (records-departments-options)
+      [:label {:for "treatments"} "Records Departments"]
+      (records-departments-options)
 
-             (submit-button {:class "btn btn-default"} "Create client")))
+      (submit-button {:class "btn btn-default"} "Create client"))))
+
+(defn index []
+  (let [clients (db/all-clients)]
+    (html
+      [:h2 "Clients"]
+      [:p
+       [:a {:href "/clients/new" :id "new-client"} "Add new"]]
+      [:br]
+      [:table.table
+       [:tr
+        [:th "First name"]
+        [:th "Last name"]
+        [:th "DOB"]
+        [:th "Date of accident"]
+        [:th "Records depts"]]
+       (for [{:keys [first_name last_name
+                     date_of_birth date_of_accident
+                     records_departments]}
+             clients]
+         [:tr
+          [:td first_name]
+          [:td last_name]
+          [:td date_of_birth]
+          [:td date_of_accident]
+          [:td (map :department_title records_departments)]])])))
