8630552593a287dba3a44577469b9caae04882d7
Ben Orenstein <ben.orenstein@gmail.com>
2014-05-12 15:41:59 -0400

    Use serif font for request. Include image for letterhead.

--- src/caliper/views/records_requests.clj
+++ src/caliper/views/records_requests.clj
@@ -15,16 +15,7 @@
 
 (defn- letterhead []
   [:section.letterhead
-   [:h2 "Joshua P. Zisson"]
-   [:h3 "Attorney at law"]
-   [:br]
-   [:section.contact-info
-    [:p "101 Tremont Street"]
-    [:p "Suite 1111"]
-    [:p "Boston, MA 02108"]
-    [:p "TEL (617) 444-9626"]
-    [:p "FAX (617) 315-8982"]
-    [:p "jpzisson@gmail.com"]]])
+   (image {:id "letterhead"} "/img/letterhead.png")])
 
 (defn- records-department-info [records-department]
   (let [{:keys [department_title hospital_name address_line_1 address_line_2]}
