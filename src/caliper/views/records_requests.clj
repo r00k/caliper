9836e3554c3f768131b0408826c13168314dfd33
Ben Orenstein <ben.orenstein@gmail.com>
2014-05-12 14:05:50 -0400

    Flesh out request more

--- src/caliper/views/records_requests.clj
+++ src/caliper/views/records_requests.clj
@@ -2,19 +2,44 @@
   (:use hiccup.core
         hiccup.element
         caliper.views.layout)
-  (:require [caliper.models.db :as db]))
+  (:require [caliper.models.db :as db]
+            [clj-time.core :as t]))
+
+(defn- today-date []
+  (let [today (t/today)]
+    [:p (str (t/month today)
+             "/"
+             (t/day today)
+             "/"
+             (t/year today))]))
+
+(defn- letterhead []
+  [:section.letterhead
+   [:h2 "Joshua P. Zisson"]
+   [:h3 "Attorney at law"]
+   [:br]
+   [:section.contact-info
+    [:p "101 Tremont Street"]
+    [:p "Suite 1111"]
+    [:p "Boston, MA 02108"]
+    [:p "TEL (617) 444-9626"]
+    [:p "FAX (617) 315-8982"]
+    [:p "jpzisson@gmail.com"]]])
+
+(defn- records-department-info
+  [records-department]
+  (let [{:keys [department_title hospital_name address_line_1 address_line_2]}
+        records-department]
+    [:section.records-department-info
+     [:p department_title]
+     [:p hospital_name]
+     [:p address_line_1]
+     [:p address_line_2]]))
 
 (defn show [client records-department]
   (html
     [:div.records-request
-     [:section.letterhead
-      [:h2 "Joshua P. Zisson"]
-      [:h3 "Attorney at law"]
-      [:br]
-      [:section.contact-info
-       [:p "101 Tremont Street"]
-       [:p "Suite 1111"]
-       [:p "Boston, MA 02108"]
-       [:p "TEL (617) 444-9626"]
-       [:p "FAX (617) 315-8982"]
-       [:p "jpzisson@gmail.com"]]]]))
+     (letterhead)
+     (today-date)
+     (records-department-info records-department)
+     ]))
