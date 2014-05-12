5e781753b47cfc0d0aeebc3c49c05402baddba43
Ben Orenstein <ben.orenstein@gmail.com>
2014-05-12 12:52:39 -0400

    Wait less

--- test/caliper/browser.clj
+++ test/caliper/browser.clj
@@ -8,7 +8,7 @@
   []
   (when (= 1 (swap! browser-count inc))
     (set-driver! {:browser :firefox})
-    (implicit-wait 3000)))
+    (implicit-wait 2000)))
 
 (defn browser-down
   "If this is the last request, shut the browser down."
